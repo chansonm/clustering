8dd75d85b85b85c1946e340643a7748d66e0e709
daniel-ifan <33721597+daniel-ifan@users.noreply.github.com>
2017-11-20 11:13:38 +0100

    Update 00-main.R

--- 00-main.R
+++ 00-main.R
@@ -7,7 +7,7 @@ source("./analysis.R")
 
 # Read data and test that they're OK
 # Todo: Replace this path with the filename you want to read
-filename <- "./for_R_no_decimals.csv"
+filename <- "./for_R_9.csv"
 #excludedColumns <- c("name",	"traditional_funding_before",	"traditional_funding_after",	"%",	"profit	blockchain",	"single",	"token_utility_product",	"only_payment",	"transaction",	"generation",	"other_usage",	"main_value",	"voting_shareholder",	"voting_product",	"voting_other",	"profit_sharing",	"info", "funding", 	"coin_offered",	"cap_money",	"same_price",	"expensive",	"start_date",	"ICO_days",	"cap_time",	"bitcoin",	"Ethereum",	"other_bc",	"bs",	"other_p",	"transaction_fee")
 #  Todo: We do not want to exclude the column "name". Therefore I commented this line below out.
 # excludedColumns <- c("name")
